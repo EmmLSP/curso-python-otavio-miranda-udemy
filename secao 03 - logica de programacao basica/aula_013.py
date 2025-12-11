e83ae3b751acef974298c66af75d0b36e8fae425
EmmLSP <aexclusivasp@gmail..com>
2025-12-11 15:57:25 -0300

    curso python - secao 03 - aula 013

---
+++ secao 03 - logica de programacao basica/aula_013.py
@@ -0,0 +1,15 @@
+# Introducao as f-strings (formatacao de strings)
+
+nome = 'Luiz Otavio'
+altura = 1.80 # metros
+peso = 95 # kg
+imc = peso / (altura ** 2)
+
+# f-strings
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu IMC é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
