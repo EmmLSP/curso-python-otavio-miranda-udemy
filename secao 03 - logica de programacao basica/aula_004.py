e0d1014f686212d3fc341b87f0f40022cec07815
EmmLSP <aexclusivasp@gmail..com>
2025-12-11 10:55:12 -0300

    curso python - secao 03 - aula 004

---
+++ secao 03 - logica de programacao basica/aula_004.py
@@ -0,0 +1,39 @@
+
+# Tipos Primitivos: int, float bool, str
+
+# Tipos int e float
+# int -> Numero inteiro
+# O tipo int representa qualquer numero
+# positivo ou negativo. int sem o sinal é considerado
+# positivo
+
+print(1) # int
+print(-1) # int
+print(0) # int
+
+# float -> Numero com ponto flutuante
+# O tipo float representa qualquer numero
+# positivo ou negativo com ponto flutuante. 
+# float sem sinal é considerado positivo.
+
+print(1.1, 10.11) # flot
+print(0.0, -1.5) # float
+
+# A funcao type mostra o tipo que o Python
+# inferiu ao valor.
+
+# Obs: tudo em Python é um objeto
+
+# <class 'str'> -> class -> objeto
+print(type('Otavio'))
+print('Otavio')
+print(type(1))
+print(1)
+print(type(0))
+print(0)
+print(type(-1))
+print(-1)
+print(type(1.1))
+print(1.1)
+print(type(1.1), type(-1.1), type(0.0))
+print(1.1, -1.1, 0.0)
