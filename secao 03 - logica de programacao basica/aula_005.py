ef6b37dee2ece8cfe1afa0d624c2ba31e2def3c3
EmmLSP <aexclusivasp@gmail..com>
2025-12-11 11:13:13 -0300

    curso python - secao 03 - aula 005

---
+++ secao 03 - logica de programacao basica/aula_005.py
@@ -0,0 +1,14 @@
+# Tipo de dado bool (boolean)
+# Ao qquestionar algo em um programa,
+# so existem duas respostas possiveis:
+# sim (True) ou nao (False).
+# Existem varios operadores para "questionar".
+# Dentre eles o ==, que é um operador logico que
+# questiona se um valor é igual a outro
+
+print(10 == 10) # Sim => True (Verdadeiro)
+print(10 == 11) # Não => False (Falso)
+print(type(True))
+print(type(False))
+print(type(10 == 10)) # expressao retorna True e tipo de dado bool
+print(type(10 == 11)) # expressao retorna False e tipo de dado bool
