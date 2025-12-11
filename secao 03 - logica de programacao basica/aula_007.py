f873dedaed60007587c829759ff67b24d3d47617
EmmLSP <aexclusivasp@gmail..com>
2025-12-11 14:18:41 -0300

    curso python - secao 03 - aula 007

---
+++ secao 03 - logica de programacao basica/aula_007.py
@@ -0,0 +1,30 @@
+# Introducao as Variaveis em Python
+
+# Variaveis sao usadas para salvar algo na memoria do computador.
+# PEP8: inicie variaveis com letras minusculas, pode usar
+# numeros e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variavel).
+# Uso: nome_variavel = expressão
+
+# nome de variavel tem quer ser bem descritivo, 
+# sobre o que esta armazenando dentro dela
+
+""" nome_completo = 'Luiz Otavio Miranda'
+soma_dois_mais_dois = 2 + 2
+int_um = int('1')
+print(int_um, type(int_um))
+int_bool = bool('1')
+print(int_bool, type(int_um))
+print(nome_completo, soma_dois_mais_dois) """
+
+nome = 'Luiz'
+idade = 30
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
+
+idade = 17
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
