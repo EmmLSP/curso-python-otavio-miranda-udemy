ca031d8a2ef2cd734f8bd3c27e13af76abdd048d
EmmLSP <aexclusivasp@gmail..com>
2025-12-11 15:04:05 -0300

    curso python - secao 03 - aula 009

---
+++ secao 03 - logica de programacao basica/aula_009.py
@@ -0,0 +1,32 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+muliplicacao = 10 * 10
+print('Multiplicação', muliplicacao)
+
+divisao = 10 / 2.2 # sempre retorna um float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisao
+print('Módulo', modulo)
+
+modulo = 25 % 5 # resto da divisao
+print('Módulo', modulo)
+
+# verificar se um numero e mutliplo de 8
+print(16 % 8 == 0) # resto 0 - True
+print(10 % 8 == 0) # resto 2 - False
+
+# verificar se um numero e par ou impar
+print(10 % 2 == 0) # resto 0 - True
+print(15 % 2 == 0) # resto 1 - False
+
