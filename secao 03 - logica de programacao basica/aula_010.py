71ea45ec560634327c1683184b822e6a07190dcd
EmmLSP <aexclusivasp@gmail..com>
2025-12-11 15:12:59 -0300

    curso python - secao 03 - aula 010

---
+++ secao 03 - logica de programacao basica/aula_010.py
@@ -0,0 +1,13 @@
+
+# Concatenacao (+) e repeticao (*) com operadores aritmeticos
+
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+nome_completo = 'Luiz' + ' ' + 'Otavio' + ' - ' + str(25) + ' ' + 'anos'
+print(nome_completo)
+
+# * -> repeticao de caracteres de string
+a_dez_vezes = 'A' * 10
+tres_vezes_luiz = 3 * 'Luiz'
+print(a_dez_vezes)
+print(tres_vezes_luiz)
