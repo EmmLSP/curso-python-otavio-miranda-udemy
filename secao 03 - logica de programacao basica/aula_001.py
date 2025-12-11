eefe67bd4034f08633d4f2962fb2ff8fcfa3da41
EmmLSP <aexclusivasp@gmail..com>
2025-12-11 09:37:48 -0300

    curso python - secao 03 - aula 001

---
+++ secao 03 - logica de programacao basica/aula_001.py
@@ -0,0 +1,18 @@
+# comentario oficial, uma cerquilha
+# le uma linha apenas
+
+'''
+recurso do Python - DOCSTRINGS
+
+Comentario com # o interpretador do Python
+nao le
+DOCSTRINGS o interpretador do Python le,
+nao e comentario
+
+O Python le o codigo da esquerda
+para direita e de cima para baixo
+'''
+
+# print() - funcao interna do python 
+# para imprimir, mostrar um output na tela 
+print(123) 
