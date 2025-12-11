4b1181df22ee1bef92f31c2339606d1312b15e62
EmmLSP <aexclusivasp@gmail..com>
2025-12-11 10:24:35 -0300

    curso python - secao 03 - aula 003

---
+++ secao 03 - logica de programacao basica/aula_003.py
@@ -0,0 +1,28 @@
+# Tipos de Dados
+
+"""
+DocString - é uma string
+usada para documentacao
+
+Python = Linguagem de programacao
+Tipo de tipagem = Dinamica / Forte
+str -> string -> texto
+Strings sao textos que estao dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Luis Otavio')
+print(1, 'Luiz "Otavio"') # passando 2 argumentos
+
+# Aspas duplas
+print("Luiz Otavio")
+print(2, "Luiz 'Otavio'") # passando 2 argumentos
+
+# Escape
+print("Luiz \"Otavio") # \ caractere de escape
+print("Luiz \"Otavio\"")
+
+# r
+print(r"Luiz \"Otavio\"")
