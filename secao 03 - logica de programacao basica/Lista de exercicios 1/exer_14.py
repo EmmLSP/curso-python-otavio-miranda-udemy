4b8a1f245fb593a332fc86c7cea2baddcb05de8f
EmmLSP <aexclusivasp@gmail..com>
2025-12-12 17:35:00 -0300

    curso python - secao 03 - lista 1 - exercicio 014

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 1/exer_14.py
@@ -0,0 +1,10 @@
+"""
+Exercicio 14
+Faca um algoritmo que leia o preco de um produto
+e mostre seu novo preco com 5% de desconto.
+"""
+
+produto_str = input('Qual é o preco do produto? R$ ')
+produto = float(produto_str)
+novo_preco = produto - (produto * 5 / 100)
+print(f'O produto que custava R${produto:.2f}, na promocao com desconto de 5%, vai custar R${novo_preco:.2f}')
