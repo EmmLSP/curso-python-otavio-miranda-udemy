a4f7a2c465918b651bfe602a04b1dc9eb5098451
EmmLSP <aexclusivasp@gmail..com>
2025-12-12 17:36:39 -0300

    curso python - secao 03 - lista 1 - exercicio 015

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 1/exer_15.py
@@ -0,0 +1,10 @@
+"""
+Exercicio 15
+Faca um algoritmo que leia o salario de um funcionario 
+e mostre seu novo salario, com 15% de aumento.
+"""
+
+salario_str = input('Qual é o salario do funcionario? R$ ')
+salario = float(salario_str)
+novo_salario = salario + (salario * 15 / 100)
+print(f'Um funcionario que ganhava R${salario:.2f}, com 15% de aumento, passa a receber R${novo_salario:.2f}')
