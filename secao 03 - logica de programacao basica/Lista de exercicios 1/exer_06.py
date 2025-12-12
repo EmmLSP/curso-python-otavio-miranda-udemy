6a7ae3e2c4e885e80d759db6f994edd9ba04ffe9
EmmLSP <aexclusivasp@gmail..com>
2025-12-12 17:15:37 -0300

    curso python - secao 03 - lista 1 - exercicio 06

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 1/exer_06.py
@@ -0,0 +1,16 @@
+"""
+Exercicio 06
+Crie um algoritmo que leia um numero e mostre
+o seu dobro, triplo e raiz quadrada.
+"""
+
+from math import sqrt
+
+numero = input('Digite um numero: ')
+
+dobro = int(numero) * 2
+triplo = int(numero) * 3
+raiz_q = sqrt(int(numero))
+print(f'O dobro {numero} vale {dobro}')
+print(f'O triplo {numero} vale {triplo}')
+print(f'A raiz quadrada de {numero} é igual a {raiz_q:.2f}')
