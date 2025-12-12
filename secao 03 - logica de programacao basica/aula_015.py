1873d5fa99bb411c60dd1e1f6fb63fa9de23dd64
EmmLSP <aexclusivasp@gmail..com>
2025-12-12 17:28:30 -0300

    curso python - secao 03 - aula 015

--- secao 03 - logica de programacao basica/aula_015.py
+++ secao 03 - logica de programacao basica/aula_015.py
@@ -6,14 +6,11 @@
 # nome = input('Qual é o seu nome? ')
 # print(f'Seu nome é {nome}')
 # print(f'Seu nome é {nome=}')
-
-numero_1 = input('Digite um numero: ')
-numero_2 = input('Digite outro numero: ')
-
 # check no meio do camainho entre os codigos
 # entrada do usuario
 
+numero_1 = input('Digite um numero: ')
+numero_2 = input('Digite outro numero: ')
 int_numero_1 = int(numero_1)
 int_numero_2 = int(numero_2)
-
 print(f'A soma dos numeros é {int_numero_1 + int_numero_2}')
