56cd24312d1198d2f33cadb29b440c1978461b8e
EmmLSP <aexclusivasp@gmail..com>
2025-12-13 16:48:15 -0300

    curso python - secao 03 - aula 018

---
+++ secao 03 - logica de programacao basica/aula_018.py
@@ -0,0 +1,25 @@
+# O Debugger do VS Code e o interpretador do Python lendo os códigos
+
+# if  / elif       / else
+# se  / se nao se  / se nao
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Codigo para a condicao 1')
+elif condicao2:
+    print('Codigo para a condicao 2')
+elif condicao3:
+    print('Codigo para a condicao 3')
+elif condicao4:
+    print('Codigo para a condicao 4')
+else:
+    print('Nenhuma condicao foi satisfeita')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
