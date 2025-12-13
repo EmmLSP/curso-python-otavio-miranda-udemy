3391ef9e89693609c3ccc273c599be9971b4c411
EmmLSP <aexclusivasp@gmail..com>
2025-12-13 14:57:38 -0300

    curso python - secao 03 - lista 1 - exercicio 34

--- secao 03 - logica de programacao basica/Lista de exercicios 1/exer_34.py
+++ secao 03 - logica de programacao basica/Lista de exercicios 1/exer_34.py
@@ -11,4 +11,4 @@ teclado e mostre:
 frase = input('Digite uma frase: ').strip().upper()
 print(f'A letra A aparece {frase.upper().count('A')} vezes na frase.')
 print(f'A primeira letra A apareceu na posicao {frase.find('A') + 1}')
-print(f'A ultima letra A apareceu na posicao {frase.rfind('A') + 1}')
+print(f'A ultima letra A apareceu na posicao {frase.rfind('A') + 1 - (frase.count(' ') - 1)}')
