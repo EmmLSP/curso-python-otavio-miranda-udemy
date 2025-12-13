f815ff93c9c88a1f24fef0bfd30a1963fa3e33bb
EmmLSP <aexclusivasp@gmail..com>
2025-12-13 16:48:43 -0300

    curso python - secao 03 - aula 019

---
+++ secao 03 - logica de programacao basica/aula_019.py
@@ -0,0 +1,27 @@
+# Operadores relacionais (de comparação) em Python
+
+"""
+Operadores de comparação (relacionais)
+------------------------------------------
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+-----------------------------------------
+"""
+
+# operadores relacionais -> return boolean: True or False
+# shell interativo do python
+# terminal -> mesma pasta do file -> python -i aula_19.py 
+# para sair do shell digitar quit()
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print(diferente)
