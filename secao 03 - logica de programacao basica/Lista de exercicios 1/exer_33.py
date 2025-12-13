cd1e6eee8564811015f50531be920c0b4a385a5f
EmmLSP <aexclusivasp@gmail..com>
2025-12-13 14:19:21 -0300

    curso python - secao 03 - lista 1 - exercicio 33

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 1/exer_33.py
@@ -0,0 +1,8 @@
+"""
+Exercicio 33
+Crie um programa que leia o nome de uma pessoa 
+e diga de ela tem "SILVA" no nome.
+"""
+
+nome_completo = input('Qual é seu nome completo? ').upper().strip()
+print('SILVA' in nome_completo)
