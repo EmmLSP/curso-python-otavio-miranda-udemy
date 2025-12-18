c8ed65c7ac7e42e52074667698e8f298c9d84513
EmmLSP <aexclusivasp@gmail..com>
2025-12-18 17:41:45 -0300

    curso python - secao 03 - lista 2 - exercicio 002

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 2/exer_002.py
@@ -0,0 +1,15 @@
+"""
+Exercicio 02
+Faca um programa que peca um valor e mostre na
+tela se o valor é positivo ou negativo.
+"""
+
+entrada = input('Digite um valor: ')
+if entrada.isdigit():
+    num = int(entrada)
+    if num >= 0:
+        print(f'{entrada} é POSITIVO')
+    else:
+        print(f'{entrada} é NEGATIVO')
+else:
+    print('Não é um numero')
