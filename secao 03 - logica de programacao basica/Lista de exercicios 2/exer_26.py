3b71448f875329e015ba94f601f10c15daabae51
EmmLSP <aexclusivasp@gmail..com>
2025-12-18 15:03:17 -0300

    curso python - secao 03 - lista 2 - exercicio 26

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 2/exer_26.py
@@ -0,0 +1,26 @@
+"""
+Exercicio 26
+Faca um programa que faca o computador pensar 
+em um numero entre 0 e 5 e peca para o usuario 
+tentardescobrir qual foi o numero escolhido pelo 
+computador.
+O programa devera escrever na tela se o usuario
+venceu ou perdeu.
+"""
+
+from random import randint
+from time import sleep
+
+print('\033[33m-=-' * 20, '\033[m')
+print(f'\033[34m{'Vou pensar em um numero entre 0 e 5. Tente adivinhar...':^60}\033[m')
+print('\033[33m-=-' * 20, '\033[m')
+
+computador = randint(0, 5)
+jogador = int(input('Em que numero eu pesei? '))
+print('\033[35mPROCESSANDO...\033[m')
+sleep(1)
+
+if jogador == computador:
+    print(f'\033[32mParabens! Voce conseguiu me vencer!\033[m')
+else:
+    print(f'\033[31mGANHEI! Eu pensei no numero {computador} e nao no {jogador}!\033[m')
