b342ffed90afc9cd82b77018eda0ba389c92b52c
EmmLSP <aexclusivasp@gmail..com>
2025-12-18 15:32:37 -0300

    curso python - secao 03 - lista 2 - exercicio 28

--- secao 03 - logica de programacao basica/Lista de exercicios 2/exer_28.py
+++ secao 03 - logica de programacao basica/Lista de exercicios 2/exer_28.py
@@ -14,11 +14,8 @@ else:
 print(f'O numero {numero} é ', end='')
 print('PAR' if numero % 2 == 0 else 'IMPAR')
 
-par_impar = False
-resultado = numero % 2 == 0
-if resultado is True:
-    par_impar = True
-if par_impar is True:
+resultado = numero % 2
+if resultado == 0:
     print(F'O numero {numero} é PAR')
 else:
     print(f'O numero {numero} é IMPAR')
