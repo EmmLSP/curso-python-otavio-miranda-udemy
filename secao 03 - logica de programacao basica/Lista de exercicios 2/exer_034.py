84a01aee4632cca95aeaf551609b6c725a04cdf4
EmmLSP <aexclusivasp@gmail..com>
2025-12-19 13:56:36 -0300

    curso python - secao 03 - lista 2 - exercicio 034

--- secao 03 - logica de programacao basica/Lista de exercicios 2/exer_034.py
+++ secao 03 - logica de programacao basica/Lista de exercicios 2/exer_034.py
@@ -13,9 +13,10 @@ casa = float(input('Valor da casa: R$ '))
 salario = float(input('Salario do comprador: R$ '))
 anos = int(input('Quantos anos de financiamento: '))
 prestacao = casa / (anos * 12)
+minimo = salario * 30 / 100
 print(f'\033[1;33mPara pegar uma casa de \033[mR${casa:,.2f}\033[1;33m', end='')
 print(f' em {anos} anos a prestacao sera de\033[m R${prestacao:.2f}')
-if prestacao <= (salario * 30 / 100): # prestacao <= 30% salario
+if prestacao <= minimo: # prestacao <= 30% salario
     print('\033[1;32mEmprestimo pode ser CONCEDIDO!\033[m')
 else:
     print('\033[1;31mEmprestimo NEGADO!\033[m')
