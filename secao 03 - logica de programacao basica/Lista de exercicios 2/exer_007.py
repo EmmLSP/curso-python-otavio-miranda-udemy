93a32836c08a0cc3ccfdabf48c06c8e301cc2933
EmmLSP <aexclusivasp@gmail..com>
2025-12-19 08:39:42 -0300

    curso python - secao 03 - lista 2 - exercicio 007

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 2/exer_007.py
@@ -0,0 +1,36 @@
+"""
+Exercicio 07
+Faca um programa que leia tres numeros e 
+mostre o maior e o menor deles.
+"""
+
+from random import randint
+
+num1 = int(input('Digite o primeiro numero: '))
+num2 = int(input('Digite o segundo numero: '))
+num3 = int(input('Digite o terceiro numero: '))
+
+# num1 = randint(1, 20)
+# num2 = randint(1, 20)
+# num3 = randint(1, 20)
+
+print(f'[{num1}, {num2}, {num3}]')
+
+if num1 >= num2 and num1 >= num3:
+    print(f'O maior numero é {num1=}')
+    if num2 <= num3:
+        print(f'O menor numero é {num2=}')
+    else:
+        print(f'O menor numero é {num3=}')
+elif num2 >= num1 and num2 >= num3:
+    print(f'O maior numero é {num2=}')
+    if num1 <= num3:
+        print(f'O menor numero é {num1=}')
+    else:
+        print(f'O menor numero é {num3=}')
+else:
+    print(f'O maior numero é {num3=}')
+    if num1 <= num2:
+        print(f'O menor numero é {num1=}')
+    else:
+        print(f'O menor numero é {num2=}')
