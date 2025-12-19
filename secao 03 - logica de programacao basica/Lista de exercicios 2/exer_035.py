a9a7e8a69c42e4a00ef8060fbe34e74e28389e3c
EmmLSP <aexclusivasp@gmail..com>
2025-12-19 14:28:19 -0300

    curso python - secao 03 - lista 2 - exercicio 035

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 2/exer_035.py
@@ -0,0 +1,28 @@
+"""
+Exercicio 035
+Escreva um programa que leia um numero inteiro
+qualquer e peca para o usuario escolher qual 
+sera a base de conversao:
+
+- 1 para binario
+- 2 para octal
+- 3 para hexadecimal
+"""
+
+num = int(input('Digite um numero inteiro: '))
+print('-' * 40)
+print('''Escolha uma das bases para conversao
+----------------------------------------
+[ 1 ] converter para BINARIO
+[ 2 ] converter para OCTAL
+[ 3 ] converter para HEXADECIMAL''')
+print('-' * 40)
+opcao = int(input('Sua opcao: '))
+if opcao == 1:
+    print(f'{num} convertido em BINARIO é igual a {str(bin(num))[2:]}')
+elif opcao == 2:
+    print(f'{num} convertido em OCTAL é igual a {str(oct(num))[2:]}')
+elif opcao == 3:
+    print(f'{num} convertido em HEXADECIMAL é igual a {str(hex(num))[2:]}')
+else:
+    print('Opcao invalida!')
