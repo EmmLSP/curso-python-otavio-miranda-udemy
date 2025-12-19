cd05143ccf90c55089fcbdeb9eba2afc34407085
EmmLSP <aexclusivasp@gmail..com>
2025-12-19 13:40:33 -0300

    curso python - secao 03 - lista 2 - exercicio 020

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 2/exer_020.py
@@ -0,0 +1,18 @@
+"""
+Exercicio 020
+Faca um programa que peca um numero e informe se o
+numero e inteiro ou decimal. Dica utilize funcao de
+arredondamento.
+"""
+
+numero = input('Digite um numero: ')
+num = 0
+if numero.isdigit():
+    num = int(numero)
+elif numero.replace('.', '').isdigit():
+    num = float(numero)
+else:
+    print('Nao é um numero')
+
+if numero.isdigit() or numero.replace('.', '').isdigit():
+    print(f'O numero {numero} é do tipo {type(num)}')
