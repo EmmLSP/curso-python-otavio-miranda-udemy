db7ae7cc122031c8aac7e5b14f91d4a3062844ff
EmmLSP <aexclusivasp@gmail..com>
2025-12-20 14:19:14 -0300

    curso python - secao 03 - aula 038

---
+++ secao 03 - logica de programacao basica/aula_038.py
@@ -0,0 +1,22 @@
+# while + while (laços internos)
+
+"""
+Repetiões
+while (enquanto)
+Executa uma acao enquanto uma condicao for verdadeira
+Loop infinito -> quando um codigo nao tem fim
+"""
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    print()
+    linha += 1
+
+print('Acabou')
