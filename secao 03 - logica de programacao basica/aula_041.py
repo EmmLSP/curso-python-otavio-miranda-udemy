4c6440141d06ff2c0b3d5cb7cef4292524711b34
EmmLSP <aexclusivasp@gmail..com>
2025-12-21 09:17:12 -0300

    curso python - secao 03  aula 041

---
+++ secao 03 - logica de programacao basica/aula_041.py
@@ -0,0 +1,45 @@
+# while / else (recurso peculiar do Python)
+
+# else e executado quando o laco vai ate o final
+# break -> interrompe o laco e o else nao e executado
+
+string = 'Valor qualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+    print(letra)
+    i += 1
+else:
+    print('O else foi executado')
+print('Fora do while')
+
+string = 'Valor qualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('O else foi executado')
+print('Fora do while')
+
+string = 'Valorqualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Nao encontrei um espaco na string')
+print('Fora do while')
