c625eadbeecd35443a9b242fe04de16ffb0abb62
EmmLSP <aexclusivasp@gmail..com>
2025-12-26 17:16:07 -0300

    curso python - secao 03 - lista 4 - exercicio 006

--- secao 03 - logica de programacao basica/Lista de exercicios 4/exer_006.py
+++ secao 03 - logica de programacao basica/Lista de exercicios 4/exer_006.py
@@ -23,12 +23,16 @@ for c in range(primeiro, decimo + razao, razao):
     print(f'{c} -> ', end='')
 print('ACABOU')
 
+print('=' * 30)
+print(f'{'10 TERMOS DE UMA PA':^30}')
+print('=' * 30)
+
 primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razao: '))
 decimo = 0
 for c in range(0, 10):
-    decimo = primeiro + (c * razao)
-    print(decimo, end=' -> ')
+    pa = primeiro + (c * razao)
+    print(pa, end=' -> ')
 print('ACABOU')
 
 print('=' * 30)
