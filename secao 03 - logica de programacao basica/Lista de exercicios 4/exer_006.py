c99edf894093b353074cccc62f030372816a2eed
EmmLSP <aexclusivasp@gmail..com>
2025-12-26 17:11:34 -0300

    curso python - secao 03 - lista 4 - exercicio 006

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 4/exer_006.py
@@ -0,0 +1,43 @@
+"""
+Exercicio 006
+Desenvolva um programa que leia o primeiro
+termo e a razao de uma PA. No final, mostre
+os 10 primeiros termos dessa progressao.
+
+formula: an = a1 + (n - 1) * r
+
+primeiro termo = 2
+razao = 3
+termos = 10
+=> 2 5 8 11 14 17 20 23 26 29
+"""
+
+print('=' * 30)
+print(f'{'10 TERMOS DE UMA PA':^30}')
+print('=' * 30)
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razao: '))
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo + razao, razao):
+    print(f'{c} -> ', end='')
+print('ACABOU')
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razao: '))
+decimo = 0
+for c in range(0, 10):
+    decimo = primeiro + (c * razao)
+    print(decimo, end=' -> ')
+print('ACABOU')
+
+print('=' * 30)
+print(f'{'10 TERMOS DE UMA PA':^30}')
+print('=' * 30)
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razao: '))
+for c in range(0, 10):
+    print(primeiro, end=' -> ')
+    primeiro += razao
+print('ACABOU')
