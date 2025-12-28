f70ea8d61a3e302a51082af52d14631b792285a5
EmmLSP <aexclusivasp@gmail..com>
2025-12-28 16:33:11 -0300

    curso python - secao 03 - lista 4 - exercicio 016

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 4/exer_016.py
@@ -0,0 +1,29 @@
+"""
+Exercicio 016
+Refaca o desafio de PA do Mundo 2, lendo o primeiro
+termo e a razao de uma PA, mostrando os 10 primeiros
+termos da progressao usando a estrutura while.
+"""
+
+# primeiro + (10 - 1) * razao
+
+print('Gerador de PA')
+print('-=' * 15)
+
+primeiro = int(input('Primeiro termo: ')) 
+razao = int(input('Razao: '))
+decimo = primeiro + (10 - 1) * razao
+while primeiro <= decimo:
+    print(primeiro, end='')
+    print(' -> ' if primeiro < decimo else '\n', end='') 
+    primeiro += razao
+
+primeiro = int(input('Primeiro termo: ')) 
+razao = int(input('Razao: '))
+termo = primeiro
+c = 0
+while c < 10:
+    print(termo, end='')
+    print(' -> ' if c < 9 else '\n', end='') 
+    termo += razao
+    c += 1
