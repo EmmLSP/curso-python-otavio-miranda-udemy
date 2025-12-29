882a784c83fbef9fa62df3fc15df88132fc1844e
EmmLSP <aexclusivasp@gmail..com>
2025-12-29 09:57:34 -0300

    curso python - secao 03 - lista 4 - exercicio 022

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 4/exer_022.py
@@ -0,0 +1,25 @@
+"""
+Exercicio 022
+Faca um programa que mostre a tabuada de varios
+numeros, um de cada vez, para cada valor digitado
+pelo usuario. O programa sera interrompido quando
+o numero solicitado for negativo.
+"""
+
+from time import sleep
+
+while True:
+    num = int(input('Quer ver a tabuada de qual valor: '))
+    if num < 0:
+        print(f'\nFinalizando programa...')
+        sleep(1.5)
+        break
+    print(f'\nTabuada do {num}:')
+    print('-' * 20)
+    for c in range(1, 11):
+        print(f'{num} x {c:2} = {num*c}')
+        sleep(0.5)
+    print('-' * 20, '\n')
+
+print('-' * 20)
+print('PROGRAMA TABUADA ENCERRADO! Volte sempre!')
