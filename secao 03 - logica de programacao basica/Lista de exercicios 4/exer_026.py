a355d52468354afe2596cb2d517a6f37a02ceff5
EmmLSP <aexclusivasp@gmail..com>
2025-12-31 14:06:35 -0300

    curso python - secao 03 - lista 4 - exercicio 026

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 4/exer_026.py
@@ -0,0 +1,39 @@
+"""
+Exercicio 026
+Crie um programa que simule o funcionamento de um
+caixa eletronico. No inicio, pergunte ao usuario
+qual sera o valor a ser sacado (numero inteiro) e
+o programa vai informar quantas cedulas de cada
+valor serao entregues.
+
+Obs.: Considere que o caixa possui cedulas de
+R$50.00, R$20.00, R$10.00 e R$1.00.
+"""
+
+print('=' * 30)
+print(f'{'BANCO CEV':^30}')
+print('=' * 30)
+
+valor = int(input('Qual valor voce quer sacar? R$ '))
+total = valor
+ced_atual = 50
+total_ced = 0
+while True:
+    if total >= ced_atual:
+        total -= ced_atual
+        total_ced += 1
+    else:
+        if total_ced > 0:
+            print(f'Total de {total_ced} cédulas de R${ced_atual:6.2f}')
+        if ced_atual == 50:
+            ced_atual = 20
+        elif ced_atual == 20:
+            ced_atual = 10
+        elif ced_atual == 10:
+            ced_atual = 1
+        total_ced = 0
+        if total == 0:
+            break
+
+print('=' * 30)
+print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
