caaf75d109f4e82fbf58826dc8b6b8676885b7ef
EmmLSP <aexclusivasp@gmail..com>
2025-12-31 11:48:07 -0300

    curso python - secao 03 - lista 4 - exercicio 024

--- secao 03 - logica de programacao basica/Lista de exercicios 4/exer_024.py
+++ secao 03 - logica de programacao basica/Lista de exercicios 4/exer_024.py
@@ -16,37 +16,41 @@ print('-' * 30)
 print(F'{'CADASTRE UMA PESSOA':^30}')
 print('-' * 30)
 
-pes = 1
-maior_18 = homens_cadast = mulheres_menor_20 = 0
+pessoa = 1
+maior_igual_18 = total_homens = total_mulheres = mulheres_menos_20 = 0
 while True:
-    print(f'Pessoa {pes}:')
-    
+    print(f'Pessoa {pessoa:03}:')
     while True:
         idade = int(input('Idade: '))
         if idade < 0:
             print('Idade precisa ser maior que zero')
             continue
         break
-    while True:
-        sexo = input('Sexo? [M/F] ').upper()
+    sexo = ' '
+    while sexo not in 'FM':
+        sexo = input('Sexo? [M/F] ').strip().upper()[0]
         if sexo not in 'MF':
             print('Sexo invalido! Digite \'F\' ou \'M\'')
             continue
-        break
     
-    if idade > 18:
-        maior_18 += 1
+    if idade >= 18:
+        maior_igual_18 += 1
+    
     if sexo == 'M':
-        homens_cadast += 1
+        total_homens += 1
+
+    if sexo == 'F':
+        total_mulheres += 1
+    
     if sexo == 'F' and idade < 20:
-        mulheres_menor_20 += 1
-    pes += 1
+        mulheres_menos_20 += 1
+    pessoa += 1
 
     print('-' * 30)
     while True:
-        resp = input('Quer continuar? [S/N] ').upper()
+        resp = input('Quer continuar? [S/N] ').strip().upper()[0]
         if resp not in 'SN':
-            print('Resposta invalida! Digite \'S\' ou \'N\'')
+            print('Entrada invalida! Digite \'S\' ou \'N\'')
             continue
         break
 
@@ -56,23 +60,30 @@ while True:
         break
 
     print('-' * 30)
-    print(F'{'CADASTRE MAIS UMA PESSOA':^30}')
+    print(F'{'CADASTRE UMA PESSOA':^30}')
     print('-' * 30)
 
-print('-=' * 5, ' FIM DO PROGRAMA ', '-=' * 5)
-if maior_18 > 0:
-    print(f'Total de pessoas com mais de 18 anos: {maior_18}')
+print(f'{' FIM DO PROGRAMA ':=^30}')
+if maior_igual_18 > 0:
+    print(f'Total de pessoas com 18 anos ou mais: {maior_igual_18}')
 else:
     print('Nenuma pessoa com mais de 18 anos foi cadastrado')
-if homens_cadast == 1:
-    print(f'Ao todo temos {homens_cadast} homem cadastrado')
-elif homens_cadast > 1:
-    print(f'Ao todo temos {homens_cadast} homens cadastrados')
+if total_homens == 1:
+    print(f'Ao todo temos {total_homens} homem cadastrado')
+elif total_homens > 1:
+    print(f'Ao todo temos {total_homens} homens cadastrados')
 else:
     print('Nenhum homem foi cadastrado')
-if mulheres_menor_20 == 1:
-    print(f'E temos {mulheres_menor_20} mulher com menos de 20 anos')
-elif mulheres_menor_20 > 1:
-    print(f'E temos {mulheres_menor_20} mulheres com menos de 20 anos')
+if total_mulheres > 0:
+    if total_mulheres == 1:
+        print(f'E temos temos {total_mulheres} mulher cadastrada')
+    else:
+        print(f'E temos {total_mulheres} mulheres cadastradas')
+    if mulheres_menos_20 == 1:
+        print(f'Sendo {mulheres_menos_20} mulher com menos de 20 anos')
+    elif mulheres_menos_20 > 1:
+        print(f'Sendo {mulheres_menos_20} mulheres com menos de 20 anos')
+    else:
+        print('E nenhuma mulher menor de 20 anos foi cadastrada')
 else:
     print('Nenhuma mulher foi cadastrada.')
