5600e328b5edbe1115b6009dca34bc2284492979
EmmLSP <aexclusivasp@gmail..com>
2025-12-31 10:54:05 -0300

    curso python - secao 03 - lista 4 - exercicio 023

--- secao 03 - logica de programacao basica/Lista de exercicios 4/exer_023.py
+++ secao 03 - logica de programacao basica/Lista de exercicios 4/exer_023.py
@@ -10,53 +10,48 @@ no final do jogo.
 from random import randint
 
 print('-=' * 15)
-print('VAMOS JOGAR PAR OU IMPAR')
+print(f'{'VAMOS JOGAR PAR OU IMPAR':^30}')
 print('-=' * 15)
 total = 0
 vitorias = 0
 while True:
-    computador = randint(0, 5)
-    jogador = int(input('Digite um valor: '))
-    opcao = input('PAR ou IMPAR? [P/I] ').upper()
-    if opcao == 'P':
-        total = jogador + computador
-        print('-' * 30)
-        print(f'Voce jogou {jogador} e o computador {computador}. ', end='')
-        print(f'Total de {total} ', end='')
+    computador = randint(0, 10)
+    while True:
+        jogador = int(input('Digite um valor: '))
+        if jogador < 0 or jogador > 10:
+            print('Valor invalido! Digite um valor entre 1 e 10')
+            continue
+        break
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = input('PAR ou IMPAR? [P/I] ').strip().upper()[0]
+        if tipo in 'PI':
+            break
+        print('Tipo invalido! Digite \'P\' ou \'I\'')
+    total = jogador + computador
+    print('-' * 30)
+    print(f'Voce jogou {jogador} e o computador {computador}. ', end='')
+    print(f'Total de {total} ', end='')
+    print('DEU PAR' if total % 2 == 0 else 'DEU IMPAR') # if simplificado
+    print('-' * 30)
+    if tipo == 'P': 
         if total % 2 == 0:
-            print('DEU PAR')
-            print('-' * 30)
             print('Voce VENCEU!')
-            print('Vamos jogar novamente...')
-            print('-=' * 15)
             vitorias += 1
         else:
-            print('DEU IMPAR')
-            print('-' * 30)
-            print('Voce PERDEU!')
-            print('-=' * 15)
+            print('Voce PERDEU!')  
             break
-    elif opcao == 'I':
-        total = jogador + computador
-        print('-' * 30)
-        print(f'Voce jogou {jogador} e o computador {computador}. ', end='')
-        print(f'Total de {total} ', end='')
+    elif tipo == 'I':
         if total % 2 == 1:
-            print('DEU IMPAR')
-            print('-' * 30)
-            print('Voce VENCEU!')
-            print('Vamos jogar novamente...')
-            print('-=' * 15)
+            print('Voce VENCEU!') 
             vitorias += 1
         else:
-            print('DEU PAR')
-            print('-' * 30)
             print('Voce PERDEU!')
-            print('-=' * 15)
             break
-    else:
-        print('Opcao invalida!')
+    print('Vamos jogar novamente...')
+    print('-=' * 15)
 
+print('-=' * 15)
 if vitorias == 1:
     print(f'GAMER OVER! Voce venceu {vitorias} vez.')
 else:
