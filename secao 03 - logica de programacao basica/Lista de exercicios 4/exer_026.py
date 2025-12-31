2ae3c19f457227e518a82626a5ea23fdf4e677c6
EmmLSP <aexclusivasp@gmail..com>
2025-12-31 14:13:49 -0300

    curso python - secao 03 - lista 4 - exercicio 026

--- secao 03 - logica de programacao basica/Lista de exercicios 4/exer_026.py
+++ secao 03 - logica de programacao basica/Lista de exercicios 4/exer_026.py
@@ -10,13 +10,13 @@ Obs.: Considere que o caixa possui cedulas de
 R$50.00, R$20.00, R$10.00 e R$1.00.
 """
 
-print('=' * 30)
+print('=' * 35)
 print(f'{'BANCO CEV':^30}')
-print('=' * 30)
+print('=' * 35)
 
 valor = int(input('Qual valor voce quer sacar? R$ '))
 total = valor
-ced_atual = 50
+ced_atual = 200
 total_ced = 0
 while True:
     if total >= ced_atual:
@@ -24,16 +24,22 @@ while True:
         total_ced += 1
     else:
         if total_ced > 0:
-            print(f'Total de {total_ced} cédulas de R${ced_atual:6.2f}')
-        if ced_atual == 50:
+            print(f'Total de {total_ced:02} cédulas de R${ced_atual:7.2f}')
+        if ced_atual == 200:
+            ced_atual = 100
+        elif ced_atual == 100:
+            ced_atual = 50
+        elif ced_atual == 50:
             ced_atual = 20
         elif ced_atual == 20:
             ced_atual = 10
         elif ced_atual == 10:
+            ced_atual = 5
+        elif ced_atual == 5:
             ced_atual = 1
         total_ced = 0
         if total == 0:
             break
 
-print('=' * 30)
+print('=' * 35)
 print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
