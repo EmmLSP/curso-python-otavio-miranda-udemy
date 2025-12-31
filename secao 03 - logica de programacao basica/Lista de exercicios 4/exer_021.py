c3768084e4d5288e574e52e1f5577bb748da2142
EmmLSP <aexclusivasp@gmail..com>
2025-12-31 10:08:04 -0300

    curso python - secao 03 - lista 4 - exercicio 021

--- secao 03 - logica de programacao basica/Lista de exercicios 4/exer_021.py
+++ secao 03 - logica de programacao basica/Lista de exercicios 4/exer_021.py
@@ -9,10 +9,9 @@ a flag).
 
 soma = cont = 0
 while True:
-    num = int(input('Digite um valor [999 para parar]: '))
+    num = int(input('Digite um valor (999 para parar): '))
     if num == 999:
         break
-    soma += num
     cont += 1
-
+    soma += num
 print(f'A soma dos {cont} valores foi {soma}!')
