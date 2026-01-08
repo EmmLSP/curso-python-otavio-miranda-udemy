4fe6e2f7527bfc4ea168b5b5d643214b63d4c757
EmmLSP <aexclusivasp@gmail..com>
2026-01-08 17:41:05 -0300

    curso python - secao 03 - lista 1 - exercicio 003

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 1/exer_003.py
@@ -0,0 +1,12 @@
+"""
+Exercicio 03
+Crie um programa que leia dois numeros e mostre
+a soma entre eles.
+"""
+
+num1_str = input('Digite um valor: ')
+num2_str = input('Digite outro valor: ')
+int_num1 = int(num1_str)
+int_num2 = int(num2_str)
+soma = int_num1 + int_num2
+print(f'A soma entre {int_num1} e {int_num2} é igual a {soma}!')
