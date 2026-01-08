7b3a16c976715d208199ce938405909fe7cc3d2b
EmmLSP <aexclusivasp@gmail..com>
2026-01-08 17:28:22 -0300

    curso python - secao 03 - lista 5 - exercicio 001

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 5/exer_001.py
@@ -0,0 +1,48 @@
+"""
+Exercicio 001
+Crie um programa que tenha uma tupla totalmente
+preenchida com uma contagem por extenso, do zero
+ate vinte.
+
+Seu programa devera ler um numero pelo teclado
+(entre 0 e 20) e mostra-lo por extenso.
+"""
+
+from time import sleep
+from os import system
+
+contagem = ('zero', 'um', 'dois', 'tres', 'quatro',
+    'cinco','seis','sete','oito','nove','dez', 
+    'onze','doze','treze','catorze','quinze',
+    'dezesseis','dezessete','dezoito','dezenove','vinte')
+
+print('-' * 30)
+while True:
+    numero = int(input('Digite um numero entre 0 e 20: '))
+    while True:
+        if numero < 0 or numero > 20:
+            numero = int(input('Tente novamente. Digite um numero entre 0 e 20: '))
+            continue
+        break
+
+    print(f'Voce digitou o numero {contagem[numero]}')
+
+    while True:
+        resp = input('Quer continuar? S/N ').upper().strip()[0]
+        if resp in 'SN':
+            break
+        print('Entreada invalida! Digite \'S\' ou \'N\'')
+    
+    if resp == 'S':
+        system('cls')
+        print('-' * 30)
+        continue
+    
+    if resp == 'N':
+        system('cls')
+        print('\nEncerrando programa...')
+        sleep(1)
+        break
+
+print('-' * 30)
+print('Fim do Programa! Volte Sempre!')
