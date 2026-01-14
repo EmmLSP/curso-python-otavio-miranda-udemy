f55743512be944bf76b140f25b22a921dcd14498
EmmLSP <aexclusivasp@gmail..com>
2026-01-14 12:33:29 -0300

    curso python - secao 03 - aula 049

---
+++ secao 03 - logica de programacao basica/aula_049.py
@@ -0,0 +1,16 @@
+# for in com tipo list
+
+"""
+For in com listas
+"""
+
+# string é um iteravel, onde o for
+# funciona com o iteravel <str>
+for letra in 'ABC':
+    print(letra)
+
+# lista iteravel, onde o for funciona
+# com iteravel <list>
+lista = ['Maria', 'Helena', 'Luiz']
+for nome in lista:
+    print(nome, type(nome))
