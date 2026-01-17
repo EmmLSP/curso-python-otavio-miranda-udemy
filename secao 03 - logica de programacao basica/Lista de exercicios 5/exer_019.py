747b16bd6f662e4df658727100e56cafd1e40bf8
EmmLSP <aexclusivasp@gmail..com>
2026-01-17 09:42:28 -0300

    curso python - secao 03 - llista 5 - exercicio 019

---
+++ secao 03 - logica de programacao basica/Lista de exercicios 5/exer_019.py
@@ -0,0 +1,21 @@
+"""
+Exercicio 019
+Faca um programa que leia nome e media
+de um aluno, guardando tambem a situacao
+em um dicionario. No final, mostre o 
+conteudo da estrutura na tela.
+"""
+
+aluno = {}
+aluno['Nome'] = input('Nome: ')
+aluno['Media'] = float(input(f'Media de {aluno['Nome']}: '))
+if aluno['Media'] >= 7:
+    aluno['Situacao'] = 'Aprovado'
+elif aluno['Media'] >= 5:
+    aluno['Situacao'] = 'Recuperacao'
+else:
+    aluno['Situacao'] = 'Reprovado'
+
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
